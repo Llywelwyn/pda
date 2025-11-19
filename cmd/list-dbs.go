4bf3d601408ef4e7d98c8315bdc86aa1a98e149d
lew <lew@ily.rs>
2025-11-19 12:45:37 +0000

    feat(listDbsCmd): lsd alias

--- cmd/list-dbs.go
+++ cmd/list-dbs.go
@@ -28,10 +28,11 @@ import (
 
 // delCmd represents the set command
 var listDbsCmd = &cobra.Command{
-	Use:   "list-dbs",
-	Short: "List all dbs.",
-	Args:  cobra.NoArgs,
-	RunE:  listDbs,
+	Use:     "list-dbs",
+	Short:   "List all dbs.",
+	Aliases: []string{"ls-dbs", "lsd"},
+	Args:    cobra.NoArgs,
+	RunE:    listDbs,
 }
 
 func listDbs(cmd *cobra.Command, args []string) error {
