f6f1376d1a5002db054a11f42281de2005c39b40
lew <lew@ily.rs>
2025-12-17 14:17:02 +0000

    fix(delCmd): error on KeyNotFound

--- cmd/del.go
+++ cmd/del.go
@@ -79,7 +79,7 @@ func del(cmd *cobra.Command, args []string) error {
 		sync:     false,
 		transact: func(tx *badger.Txn, k []byte) error {
 			if err := tx.Delete(k); errors.Is(err, badger.ErrKeyNotFound) {
-				return nil
+				return fmt.Errorf("cannot remove '%s': No such key", args[0])
 			}
 			return fmt.Errorf("cannot remove '%s': %v", args[0], err)
 		},
